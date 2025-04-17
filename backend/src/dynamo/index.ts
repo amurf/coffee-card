062967cb7bae89c67e7a98da9624d73f6e07434e
amurf <ash.d.murphy@gmail.com>
2025-04-18 00:20:53 +1000

    wip dynamo client

---
+++ backend/src/dynamo/index.ts
@@ -0,0 +1,49 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import {
+  DynamoDBDocumentClient,
+  QueryCommand,
+  PutCommand,
+  PutCommandOutput,
+} from "@aws-sdk/lib-dynamodb"
+import { LoyaltyCard } from "@coffee-card/shared"
+
+const client = new DynamoDBClient({})
+const docClient = DynamoDBDocumentClient.from(client)
+
+const TABLE_NAME = "CoffeeCardData" // TODO Environment variable
+const TABLE_INDEXES = {
+  GET_BY_CARD_ID: "getByCardId",
+} as const
+
+export const insertData = async <T extends Record<string, any>>(
+  data: T,
+): Promise<PutCommandOutput> => {
+  const command = new PutCommand({
+    TableName: TABLE_NAME,
+    Item: data,
+  })
+
+  const response = await docClient.send(command)
+  return response
+}
+
+export const getCard = async (
+  cardId: LoyaltyCard["cardId"],
+): Promise<LoyaltyCard | null> => {
+  const command = new QueryCommand({
+    TableName: TABLE_NAME,
+    IndexName: TABLE_INDEXES.GET_BY_CARD_ID,
+    KeyConditionExpression: "cardId = :cardId",
+    ExpressionAttributeValues: {
+      ":cardId": cardId,
+    },
+  })
+
+  const response = await docClient.send(command)
+
+  if (response.Items?.length) {
+    return response.Items[0] as LoyaltyCard
+  }
+
+  return null
+}
--- backend/src/scripts/dynamo-seed-dev.ts
+++ backend/src/scripts/dynamo-seed-dev.ts
@@ -1,40 +1,21 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
-import {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-} from "@aws-sdk/lib-dynamodb"
-
+import { insertData, getCard } from "../dynamo"
 import { LoyaltyCard, StoreProfile } from "@coffee-card/shared"
-const client = new DynamoDBClient({})
-const docClient = DynamoDBDocumentClient.from(client)
 
-// TODO: client lib wrapper.
-const insertData = async <T extends Record<string, any>>(
-  tableName: string,
-  data: T,
-) => {
-  const command = new PutCommand({
-    TableName: tableName,
-    Item: data,
-  })
-
-  const response = await docClient.send(command)
-  return response
-}
-
-await insertData<StoreProfile>("StoreProfileWithLoyaltyCards", {
+await insertData({
   PK: "STORE#CoffeeLads",
   SK: "PROFILE",
   storeName: "Coffee Lads",
   location: "Melbourne",
-})
+} as StoreProfile)
 
-await insertData<LoyaltyCard>("StoreProfileWithLoyaltyCards", {
+await insertData({
   PK: "STORE#CoffeeLads",
   SK: "CARD",
   cardId: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
   storeName: "Coffee Lads",
   issueDate: "2025-04-16",
   coffeeCount: 0,
-})
+} as LoyaltyCard)
+
+const card = await getCard("f47ac10b-58cc-4372-a567-0e02b2c3d479")
+console.log("Card:", card)
