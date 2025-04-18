89fbf004d71e32124caf119a3c1e9233b501f655
amurf <ash.d.murphy@gmail.com>
2025-04-18 20:57:48 +1000

    add new deploy all script

---
+++ backend/deploy/deploy-all-lambda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# For all files in the src/api directory, call ./deploy-lambda.sh $fileNameWithoutExtension
+SOURCE_DIR="./dist/backend/src/api"
+FILES=$(find "$SOURCE_DIR" -type f -name "*.js")
+for FILE in $FILES; do
+  # Get the file name without the extension
+  FILE_NAME=$(basename "$FILE" .js)
+  ./deploy-lambda.sh "$FILE_NAME"
+done
