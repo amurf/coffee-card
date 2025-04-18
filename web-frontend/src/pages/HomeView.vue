5df05dd681c379f9a2476f0d9ee7e0c152a09be7
amurf <ash.d.murphy@gmail.com>
2025-04-18 15:35:19 +1000

    remove initial code we dont want, basic tanstack query setup

---
+++ web-frontend/src/pages/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useQuery } from "@tanstack/vue-query"
+// TODO: sort out how to get api url into app
+const apiUrl = "https:/tbc"
+const fetchData = async () => {
+  const response = await fetch(apiUrl)
+  if (!response.ok) {
+    throw new Error("Network response was not ok")
+  }
+  return response.json()
+}
+
+const { data, error, isLoading } = useQuery({
+  queryKey: ["data"],
+  queryFn: fetchData,
+})
+</script>
+
+<template>
+  <main>
+    <h1>Home page</h1>
+    <pre>{{ data.message }}</pre>
+    <p v-if="error">{{ error }}</p>
+  </main>
+</template>
