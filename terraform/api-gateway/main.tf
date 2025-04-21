fd754dd7f9cd7f271172d10f953f4e760e64310d
amurf <ash.d.murphy@gmail.com>
2025-04-21 23:42:18 +1000

    cors for post

--- terraform/api-gateway/main.tf
+++ terraform/api-gateway/main.tf
@@ -4,7 +4,7 @@ resource "aws_apigatewayv2_api" "coffee_card_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_methods = ["GET", "OPTIONS"]
+    allow_methods = ["GET", "POST", "OPTIONS"]
     allow_origins = ["*"]
     allow_headers = ["*"]
   }
