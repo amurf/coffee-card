f494ada7fccd993808195e3fc15ef84446ff7662
amurf <ash.d.murphy@gmail.com>
2025-04-23 16:37:41 +1000

    setup QR code scanner / PoC of POS app

--- shopify-pos-app/extensions/redeem-coffee/src/Modal.tsx
+++ shopify-pos-app/extensions/redeem-coffee/src/Modal.tsx
@@ -2,58 +2,92 @@ import React, { useEffect, useState } from "react"
 
 import {
   Text,
+  Section,
   Screen,
   ScrollView,
   Navigator,
   reactExtension,
+  useScannerDataSubscription,
+  CameraScanner,
 } from "@shopify/ui-extensions-react/point-of-sale"
 
 // Proof of concept to ensure it can call the API and get a response
 // Will create an API client in the shared package so we aren't duplicating this in FE + POS
-import { LoyaltyCard } from "@coffee-card/shared"
+import type { LoyaltyCard } from "@coffee-card/shared"
+import { getCardById } from "@coffee-card/shared"
+import { Card } from "@shopify/polaris"
 
-const Modal = () => {
-  const [card, setCard] = useState<LoyaltyCard | null>(null)
-  const [loading, setLoading] = useState(true)
+const Loading = ({
+  loading,
+  children,
+}: {
+  loading: boolean
+  children: React.ReactNode
+}) => {
+  return loading ? <Text>Loading...</Text> : <>{children}</>
+}
 
-  const cardId = "2fa5fe9b-14b6-43d2-9d42-10d91e2592f0"
-  const apiBaseUrl =
-    "https://rstij0f9ll.execute-api.ap-southeast-2.amazonaws.com/dev"
-  const apiUrl = `${apiBaseUrl}/cards/${cardId}`
-  const fetchData = async (): Promise<LoyaltyCard> => {
-    const response = await fetch(apiUrl)
-    if (!response.ok) {
-      throw new Error("Network response was not ok")
-    }
-    return response.json()
+const CardNotFound = () => {
+  return <Text>Error loading card</Text>
+}
+
+const CardDetails = ({ card }: { card: LoyaltyCard | null }) => {
+  if (!card) {
+    return <CardNotFound />
   }
 
+  return (
+    <Text>
+      Card ID: {card.cardId}
+      {"\n"}
+      Store name: {card.storeName}
+    </Text>
+  )
+}
+
+const Scanner = ({ onScan }: { onScan: (scannedData: string) => void }) => {
+  const { data } = useScannerDataSubscription()
+
   useEffect(() => {
-    const fetchCard = async () => {
-      try {
-        const cardData = await fetchData()
-        setCard(cardData)
-      } catch (error) {
-        console.error("Error fetching card:", error)
-      } finally {
-        setLoading(false)
-      }
+    if (data) {
+      onScan(data)
     }
-    fetchCard()
-  }, [])
+  }, [data, onScan])
+
+  return <CameraScanner />
+}
+
+const Modal = () => {
+  const [cardId, setCardId] = useState<string | null>(null)
+  const [card, setCard] = useState<LoyaltyCard | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  // const cardId = "2fa5fe9b-14b6-43d2-9d42-10d91e2592f0"
+
+  const onScan = (cardId: string) => {
+    setCardId(cardId)
+    setLoading(true)
+
+    getCardById(cardId)
+      .then((cardData) => setCard(cardData))
+      .catch((error) => console.error("Error fetching card:", error))
+      .finally(() => setLoading(false))
+  }
 
   // const card = await getCardById("2fa5fe9b-14b6-43d2-9d42-10d91e2592f0")
   return (
     <Navigator>
       <Screen name="HelloWorld" title="Hello World!">
         <ScrollView>
-          {loading ? (
-            <Text>Loading...</Text>
-          ) : !card ? (
-            <Text>Card not found</Text>
-          ) : (
-            <Text>Card ID: {card.cardId}</Text>
+          {cardId && (
+            <Section title="Card Details">
+              <Loading loading={loading}>
+                <CardDetails card={card} />
+              </Loading>
+            </Section>
           )}
+
+          {!cardId && <Scanner onScan={onScan} />}
         </ScrollView>
       </Screen>
     </Navigator>
